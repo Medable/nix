f8f6cf0ae0329cafd0e27ec855ac2e34726538a5
jacobi petrucciani <jacobi.petrucciani@medable.com>
2026-01-21 09:13:16 -0500

    add back nodejs_16

--- mods/node.nix
+++ mods/node.nix
@@ -12,8 +12,24 @@ rec {
     })
     { }).nodejs-14_x;
 
+  # we need nodejs 16 back
+  nodejs-16_x = nodejs-16_20_0;
+  nodejs-16_20_0 = (import
+    (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/eb5e3816eafb761d3db8323bbd0f6d2737cf77d7.tar.gz";
+      sha256 = "0h154c6ybmqaidv8zzi8lbrn961psq79q2aqbq8pna3jcym2n5r4";
+    })
+    {
+      config = {
+        permittedInsecurePackages = [
+          "nodejs-16.20.2"
+        ];
+      };
+    }).nodejs_16;
+
   custom = prev.custom // {
-    inherit nodejs-14_x;
+    inherit nodejs-14_x nodejs-16_x;
     nodejs_14 = nodejs-14_20_0;
+    nodejs_16 = nodejs-16_20_0;
   };
 }
