bbf729bbd7e144392b3c9dfe3fdf9041bfe9188c
jacobi petrucciani <jacobi.petrucciani@medable.com>
2026-01-21 16:26:46 -0500

    add nodejs_18

--- mods/node.nix
+++ mods/node.nix
@@ -3,7 +3,6 @@ with prev;
 rec {
   inherit (stdenv) isLinux isDarwin isAarch64;
 
-  # we need nodejs 14 back
   nodejs-14_x = nodejs-14_20_0;
   nodejs-14_20_0 = (import
     (builtins.fetchTarball {
@@ -12,7 +11,6 @@ rec {
     })
     { }).nodejs-14_x;
 
-  # we need nodejs 16 back
   nodejs-16_x = nodejs-16_20_0;
   nodejs-16_20_0 = (import
     (builtins.fetchTarball {
@@ -27,9 +25,18 @@ rec {
       };
     }).nodejs_16;
 
+  nodejs-18_x = nodejs-18_20_6;
+  nodejs-18_20_6 = (import
+    (builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/4457f3606c598ae825fb4b185b65a464d9d07a16.tar.gz";
+      sha256 = "0padq6kva9s2d0xsn7alsivswl4g37a781yyv59vlwf4iv2i1n4l";
+    })
+    { }).nodejs_18;
+
   custom = prev.custom // {
-    inherit nodejs-14_x nodejs-16_x;
+    inherit nodejs-14_x nodejs-16_x nodejs-18_x;
     nodejs_14 = nodejs-14_20_0;
     nodejs_16 = nodejs-16_20_0;
+    nodejs_18 = nodejs-18_20_6;
   };
 }
